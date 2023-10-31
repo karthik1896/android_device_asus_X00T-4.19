fa12ed5edea9813ceb4696a0f156f9b3a85d31fb
karthik1896 <karthik1896@outlook.com>
2023-11-01 05:38:59 +0700

    X00T: Decommonize
    * Imported from https://github.com/LineageOS/android_device_asus_X00TD
    
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

---
+++ lineage_X00T.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_X00T
+PRODUCT_DEVICE := X00T
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=X00T \
+    PRODUCT_DEVICE=ASUS_X00T \
+    PRODUCT_NAME=WW_X00T \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys"
