2f0c9deb5e0174ac17333f70d7cefb59d510b54d
karthik1896 <karthik1896@outlook.com>
2023-11-02 15:11:57 +0100

    X00T: Adapt to VoltageOS
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00T.mk
+    $(LOCAL_DIR)/voltage_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00T-eng \
-    lineage_X00T-userdebug \
-    lineage_X00T-user
+    voltage_X00T-eng \
+    voltage_X00T-userdebug \
+    voltage_X00T-user
---
+++ voltage_X00T.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 2160
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Voltage stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+
+# Official-ify
+VOLTAGE_BUILD_TYPE := OFFICIAL
+
+# Inherit from X00T device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00T
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := voltage_X00T
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00T
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=X00T \
+    PRODUCT_DEVICE=X00T \
+    PRODUCT_NAME=WW_X00T \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
